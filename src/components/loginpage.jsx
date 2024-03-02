83032c23a83b681bb8b52c476b7b4e9360d1d9a2
[Lezoson] <[lezoson@outlook.com]>
2024-03-02 13:59:39 +0530

    changed login page

--- src/components/loginpage.jsx
+++ src/components/loginpage.jsx
@@ -23,12 +23,16 @@ function Login()
         <>
         <div className='container'>
                 <div className='wrapper'>
-{/* header starts here                 */}
+
+{/* header starts here*/}
+
                 <div className='header'>
                     <h1>Hello Champ!</h1>
                     <p>Please Login into your account</p>
                 </div>
- {/* form field starts here        */}
+
+ {/* form field starts here */}
+
  <form action="#" method='post'>
                 <input type="text" className='email' id='email' placeholder='Email address' required/>
                 <input type={passwordType} className='password' id='password' placeholder='Password' onChange={handlePasswordChange} value={passwordInput} required/>
@@ -37,11 +41,14 @@ function Login()
                      :<span onClick={togglePassword}><BiSolidHide className='passicons'  onClick={togglePassword}/>Hide</span> }
                 
 {/* Buttons start here */}
+
                 <div className='buttons'>
                 <input type='submit' className='loginbtn' value='Login'/>
                 </div>
                 </form>
-{/* forgot password section                */}
+
+{/* forgot password section*/}
+
             <div className='forgot-pass'>
                 <p><a href="#">Forgot password?</a></p>
             </div>
